8fff36a3fa7f2e5e12a519ccfe420583f19339a3
Student <example@example.com>
2019-07-05 11:14:22 -0700

    initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -63,7 +63,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       template:'template.ejs',
       appMountId: 'react-app-root',
-      title: 'React Help Queue',
+      title: 'React Tap Room',
       filename: resolve(__dirname, "build", "index.html"),
     }),
   ]
