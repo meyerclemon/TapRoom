8898cd0d71d8bddd7f7e9f8f379d67eabd22bffa
Student <example@example.com>
2019-07-07 14:46:23 -0700

    add the initial age verification state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Header from './Header'
 import KegList from './KegList'
 import { Switch, Route } from 'react-router-dom'
-import NewKegForm from './NewKegForm'
+// import NewKegForm from './NewKegForm'
+import PatronAgeControl from './PatronAgeControl'
 import Error404 from './Error404'
 
 
@@ -13,7 +14,8 @@ function App(){
       <Header/>
       <Switch>
         <Route exact path='/' component={KegList} />
-        <Route path='/newkeg' component={NewKegForm} />
+        {/* <Route path='/newkeg' component={NewKegForm} /> */}
+        <Route path='/agecontrol' component={PatronAgeControl} />
         <Route component={Error404} />
       </Switch>
 
